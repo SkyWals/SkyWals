7c9ec35058014e269572d56fb42e0746bf969e94
npcprogramador <gwalisonls@gmail.com>
2023-09-21 14:58:50 -0300

    update

--- public/overlay.js
+++ public/overlay.js
@@ -1,12 +1,1 @@
 //configurando-janela-flutuante
-const openOverlay = document.getElementById("openoverlay")
-const closeOverlay = document.getElementById("closeoverlay")
-const overlay = document.getElementById("overlay")
-
-openOverlay.onclick = function () {
-    overlay.showModal()
-}
-
-closeOverlay.onclick = function () {
-    overlay.close()
-}
