2d414deeb266b8fdfd1129462b5713bfb092645f
npcprogramador <gwalisonls@gmail.com>
2023-09-22 14:53:25 -0300

    update

--- public/overlay.js
+++ public/overlay.js
@@ -1,1 +1,16 @@
 //configurando-janela-flutuante
+(function () {
+    var updateButton = document.getElementById("updateDetails");
+    var cancelButton = document.getElementById("cancel");
+    var favDialog = document.getElementById("favDialog");
+  
+    // O botão Update abre uma Dialog
+    updateButton.addEventListener("click", function () {
+      favDialog.showModal();
+    });
+  
+    // O botão cancelButtom fecha uma Dialog
+    cancelButton.addEventListener("click", function () {
+      favDialog.close();
+    });
+  })();
