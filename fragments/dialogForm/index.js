fb8bb17badc4ae20fbb7d62ab979cb919e7aa38e
npcwalison <gwalisonls@gmail.com>
2023-09-28 13:30:56 -0300

    update

--- fragments/dialogForm/index.js
+++ fragments/dialogForm/index.js
@@ -3,6 +3,7 @@
   var openDialogForm = document.getElementById("openDialogForm");
   var closeDialogForm = document.getElementById("closeDialogForm");
   var portifolioForm = document.getElementById("portifolioForm");
+  var sendForm = document.getElementById('sendForm')
 
   // O botão Update abre uma Dialog
   openDialogForm.addEventListener("click", function () {
@@ -13,4 +14,10 @@
   closeDialogForm.addEventListener("click", function () {
     portifolioForm.close();
   });
+
+  //enviar dados do formulário
+  sendForm.addEventListener('click', function() {
+    document.getElementById('meuFormulario').submit();
+});
 })();
+
